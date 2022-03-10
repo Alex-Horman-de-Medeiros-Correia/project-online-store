64a1af98849dbbc571bf580eb15e33cf85b7e3c0
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-03-10 16:55:22 -0300

    adiciona lista de categorias na pagina inicial requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import List from '../components/List';
 
 class Home extends React.Component {
   render() {
     return (
-      <header>
-        <input type="text" />
-        <h2 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h2>
-      </header>
+      <>
+        <header>
+          <input type="text" />
+          <h2 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h2>
+        </header>
+        <List />
+      </>
     );
   }
 }
