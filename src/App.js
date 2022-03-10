5abe0fd71f4fb8382516a77a3818e2377cc9e619
alansouza1 <alansouza6000@gmail.com>
2022-03-10 17:27:18 -0300

    requisito 3 coloca o route no content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Content from './components/Content';
-import ShoppingCart from './pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Content />
-        <Route path="/shopping-cart" component={ ShoppingCart } />
       </BrowserRouter>
     );
   }
